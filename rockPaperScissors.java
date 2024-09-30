f5fd05403991492f4caac006eaae34925bc63d58
Zephyros <zxphyros@gmail.com>
2024-09-30 08:17:03 -0400

    do the things

--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -2,28 +2,24 @@ import java.util.Scanner;
 
 public class rockPaperScissors {
     public static void main(String[] args){
+        //init var
         Scanner scan = new Scanner(System.in);
+        String[] OPTIONS = {"Rock", "Paper", "Scissors"};
         //input/output player1
         System.out.println("PLAYER 1: Enter Rock/Paper/Scissors");
         String playerOneInput = scan.next();
-
-        //error check
-        while (playerOneInput.equals("X")){
-            System.out.println("Sorry, that isn't a valid input. Try again.");
-            playerOneInput = scan.next();
-            playerOneInput = misc.rockPaperScissorsCase(playerOneInput); //TODO: bruh just use equalsignorecase instead of this dumb method bs
-        }
+        
 
         //input/output player2
         System.out.println("PLAYER 2: Enter Rock/Paper/Scissors");
         String playerTwoInput = scan.next();
-        playerTwoInput = misc.rockPaperScissorsCase(playerTwoInput);
+        playerTwoInput = rockPaperScissorsCase(playerTwoInput);
 
         //error check
         while (playerTwoInput.equals("X")){
             System.out.println("Sorry, that isn't a valid input. Try again.");
             playerTwoInput = scan.next();
-            playerTwoInput = misc.rockPaperScissorsCase(playerTwoInput);
+            playerTwoInput = rockPaperScissorsCase(playerTwoInput);
         }
 
 
@@ -80,5 +76,20 @@ public class rockPaperScissors {
                 }
                 break;
         }
+    }
+
+    //allows players to type rock/paper/scissors regardless of case
+    //"why not use equalsignorecase in main" BECAUSE ITS A SWITCH STATEMENT im too tired to rewrite it
+    public static String rockPaperScissorsCase(String input){
+        if(input.equalsIgnoreCase("Rock")){
+            input = "R";
+        }else if(input.equalsIgnoreCase("Paper")){
+            input = "P";
+        }else if(input.equalsIgnoreCase("Scissors")){
+            input = "S";
+        }else{
+            input = "X";
+        }
+        return input;
     }
 }
