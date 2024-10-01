312a96cbaca1d8dc99e471a37d78d3ee9526d86e
Zephyrosickness <zxphyros@gmail.com>
2024-09-30 20:03:00 -0400

    we done over here.

--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -1,95 +1,80 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class rockPaperScissors {
-    public static void main(String[] args){
-        //init var
-        Scanner scan = new Scanner(System.in);
-        String[] OPTIONS = {"Rock", "Paper", "Scissors"};
+    public static void main(String[] args) {
+
         //input/output player1
         System.out.println("PLAYER 1: Enter Rock/Paper/Scissors");
-        String playerOneInput = scan.next();
-        
+        String playerOneInput = validCheck();
+
 
         //input/output player2
         System.out.println("PLAYER 2: Enter Rock/Paper/Scissors");
-        String playerTwoInput = scan.next();
-        playerTwoInput = rockPaperScissorsCase(playerTwoInput);
+        String playerTwoInput = validCheck();
 
-        //error check
-        while (playerTwoInput.equals("X")){
-            System.out.println("Sorry, that isn't a valid input. Try again.");
-            playerTwoInput = scan.next();
-            playerTwoInput = rockPaperScissorsCase(playerTwoInput);
-        }
+        //PLAYER 1 ROCK
+        if (playerOneInput.equalsIgnoreCase("Rock")) {
 
+            if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                //PLAYER 1 ROCK PLAYER 2 ROCK
+                System.out.println("Tie!");
+            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                //PLAYER 1 ROCK PLAYER 2 PAPER
+                System.out.println("Player 2 wins!");
+            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                //PLAYER 1 ROCK PLAYER 2 SCISSORS
+                System.out.println("Player 1 wins!");
+            }
 
-        switch(playerOneInput){
-            //PLAYER 1 ROCK
-            case "R":
-                switch (playerTwoInput) {
-                    case "R":
-                        //PLAYER 1 ROCK PLAYER 2 ROCK
-                        System.out.println("Tie!");
-                        break;
-                    case "P":
-                        //PLAYER 1 ROCK PLAYER 2 PAPER
-                        System.out.println("Player 2 wins!");
-                        break;
-                    case "S":
-                        //PLAYER 1 ROCK PLAYER 2 SCISSORS
-                        System.out.println("Player 1 wins!");
-                        break;
-                }
-                break;
             //PLAYER 1 PAPER
-            case "P":
-                switch (playerTwoInput) {
-                    case "R":
-                        //PLAYER 1 PAPER PLAYER 2 ROCK
-                        System.out.println("Player 1 wins!");
-                        break;
-                    case "P":
-                        //PLAYER 1 PAPER PLAYER 2 PAPER
-                        System.out.println("Tie!");
-                        break;
-                    case "S":
-                        //PLAYER 1 PAPER PLAYER 2 SCISSORS
-                        System.out.println("Player 2 wins!");
-                        break;
-                }
-                break;
+        } else if (playerOneInput.equalsIgnoreCase("Paper")) {
+
+            if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                //PLAYER 1 PAPER PLAYER 2 ROCK
+                System.out.println("Player 1 wins!");
+            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                //PLAYER 1 PAPER PLAYER 2 PAPER
+                System.out.println("Tie!");
+            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                //PLAYER 1 PAPER PLAYER 2 SCISSORS
+                System.out.println("Player 2 wins!");
+            }
+
             //PLAYER 1 SCISSORS
-            case "S":
-                switch (playerTwoInput) {
-                    case "R":
-                        //PLAYER 1 SCISSORS PLAYER 2 ROCK
-                        System.out.println("Player 2 wins!");
-                        break;
-                    case "P":
-                        //PLAYER 1 SCISSORS PLAYER 2 PAPER
-                        System.out.println("Player 1 wins!");
-                        break;
-                    case "S":
-                        //PLAYER 1 SCISSORS PLAYER 2 SCISSORS
-                        System.out.println("Tie!");
-                        break;
-                }
-                break;
+        } else if (playerOneInput.equalsIgnoreCase("Scissors")) {
+
+            if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                //PLAYER 1 SCISSORS PLAYER 2 ROCK
+                System.out.println("Player 2 wins!");
+            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                //PLAYER 1 SCISSORS PLAYER 2 PAPER
+                System.out.println("Player 1 wins!");
+            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                //PLAYER 1 SCISSORS PLAYER 2 SCISSORS
+                System.out.println("Tie!");
+            }
         }
     }
 
-    //allows players to type rock/paper/scissors regardless of case
-    //"why not use equalsignorecase in main" BECAUSE ITS A SWITCH STATEMENT im too tired to rewrite it
-    public static String rockPaperScissorsCase(String input){
-        if(input.equalsIgnoreCase("Rock")){
-            input = "R";
-        }else if(input.equalsIgnoreCase("Paper")){
-            input = "P";
-        }else if(input.equalsIgnoreCase("Scissors")){
-            input = "S";
-        }else{
-            input = "X";
-        }
+    public static String validCheck(){
+        //init var
+        String[] OPTIONS = {"Rock", "Paper", "Scissors"};
+        Scanner scan = new Scanner(System.in);
+        boolean valid = false;
+        String input;
+
+        do{
+            input = scan.next();
+            for(String i:OPTIONS){
+                if(input.equalsIgnoreCase(i)){
+                    valid = true;
+                }
+            }
+            if(!valid){
+                System.out.println("Invalid input. Try again.");
+            }
+        }while(!valid);
+
         return input;
     }
 }
