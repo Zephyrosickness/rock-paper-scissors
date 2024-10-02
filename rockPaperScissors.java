f3a310050bed65261c011dff8f169faf1cc45de5
Zephyros <zxphyros@gmail.com>
2024-10-02 09:43:33 -0400

    add play-again loop

--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -2,76 +2,94 @@ import java.util.*;
 
 public class rockPaperScissors {
     public static void main(String[] args) {
+        //init var
+        final String[] PLAYER_OPTIONS = {"Rock", "Paper", "Scissors"};
+        final String[] EXIT_OPTIONS = {"Yes","No"};
+        boolean exit = false;
+        do {
+            //input/output player1
+            System.out.println("PLAYER 1: Enter Rock/Paper/Scissors");
+            String playerOneInput = validCheck(PLAYER_OPTIONS);
 
-        //input/output player1
-        System.out.println("PLAYER 1: Enter Rock/Paper/Scissors");
-        String playerOneInput = validCheck();
 
+            //input/output player2
+            System.out.println("PLAYER 2: Enter Rock/Paper/Scissors");
+            String playerTwoInput = validCheck(PLAYER_OPTIONS);
 
-        //input/output player2
-        System.out.println("PLAYER 2: Enter Rock/Paper/Scissors");
-        String playerTwoInput = validCheck();
+            //PLAYER 1 ROCK
+            if (playerOneInput.equalsIgnoreCase("Rock")) {
 
-        //PLAYER 1 ROCK
-        if (playerOneInput.equalsIgnoreCase("Rock")) {
+                if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                    //PLAYER 1 ROCK PLAYER 2 ROCK
+                    System.out.println("Tie!");
+                } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                    //PLAYER 1 ROCK PLAYER 2 PAPER
+                    System.out.println("Player 2 wins!");
+                } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                    //PLAYER 1 ROCK PLAYER 2 SCISSORS
+                    System.out.println("Player 1 wins!");
+                }
 
-            if (playerTwoInput.equalsIgnoreCase("Rock")) {
-                //PLAYER 1 ROCK PLAYER 2 ROCK
-                System.out.println("Tie!");
-            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
-                //PLAYER 1 ROCK PLAYER 2 PAPER
-                System.out.println("Player 2 wins!");
-            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
-                //PLAYER 1 ROCK PLAYER 2 SCISSORS
-                System.out.println("Player 1 wins!");
+                //PLAYER 1 PAPER
+            } else if (playerOneInput.equalsIgnoreCase("Paper")) {
+
+                if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                    //PLAYER 1 PAPER PLAYER 2 ROCK
+                    System.out.println("Player 1 wins!");
+                } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                    //PLAYER 1 PAPER PLAYER 2 PAPER
+                    System.out.println("Tie!");
+                } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                    //PLAYER 1 PAPER PLAYER 2 SCISSORS
+                    System.out.println("Player 2 wins!");
+                }
+
+                //PLAYER 1 SCISSORS
+            } else if (playerOneInput.equalsIgnoreCase("Scissors")) {
+
+                if (playerTwoInput.equalsIgnoreCase("Rock")) {
+                    //PLAYER 1 SCISSORS PLAYER 2 ROCK
+                    System.out.println("Player 2 wins!");
+                } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
+                    //PLAYER 1 SCISSORS PLAYER 2 PAPER
+                    System.out.println("Player 1 wins!");
+                } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
+                    //PLAYER 1 SCISSORS PLAYER 2 SCISSORS
+                    System.out.println("Tie!");
+                }
             }
 
-            //PLAYER 1 PAPER
-        } else if (playerOneInput.equalsIgnoreCase("Paper")) {
-
-            if (playerTwoInput.equalsIgnoreCase("Rock")) {
-                //PLAYER 1 PAPER PLAYER 2 ROCK
-                System.out.println("Player 1 wins!");
-            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
-                //PLAYER 1 PAPER PLAYER 2 PAPER
-                System.out.println("Tie!");
-            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
-                //PLAYER 1 PAPER PLAYER 2 SCISSORS
-                System.out.println("Player 2 wins!");
+            System.out.println("Play again? [Y/N]");
+            String option = validCheck(EXIT_OPTIONS);
+            if (option.equalsIgnoreCase("No")) {
+                exit = true;
             }
 
-            //PLAYER 1 SCISSORS
-        } else if (playerOneInput.equalsIgnoreCase("Scissors")) {
-
-            if (playerTwoInput.equalsIgnoreCase("Rock")) {
-                //PLAYER 1 SCISSORS PLAYER 2 ROCK
-                System.out.println("Player 2 wins!");
-            } else if (playerTwoInput.equalsIgnoreCase("Paper")) {
-                //PLAYER 1 SCISSORS PLAYER 2 PAPER
-                System.out.println("Player 1 wins!");
-            } else if (playerTwoInput.equalsIgnoreCase("Scissors")) {
-                //PLAYER 1 SCISSORS PLAYER 2 SCISSORS
-                System.out.println("Tie!");
-            }
-        }
+        }while (!exit);
     }
 
-    public static String validCheck(){
+    public static String validCheck(final String[] OPTIONS){
         //init var
-        String[] OPTIONS = {"Rock", "Paper", "Scissors"};
         Scanner scan = new Scanner(System.in);
         boolean valid = false;
-        String input;
+        String input = scan.next();
 
         do{
-            input = scan.next();
             for(String i:OPTIONS){
-                if(input.equalsIgnoreCase(i)){
+                if(input.equalsIgnoreCase(i)){ //runs through all 3 options and sees if input is equal to them
+                    //unfortunately when casting an array to an arraylist so you
                     valid = true;
+                }else{
+                    if (input.equalsIgnoreCase(String.valueOf(i.charAt(0)))){
+                        //EX: if player inputs R, automatically correct to "Rock"
+                        input = i;
+                        valid = true;
+                    }
                 }
             }
             if(!valid){
                 System.out.println("Invalid input. Try again.");
+                input = scan.next();
             }
         }while(!valid);
 
